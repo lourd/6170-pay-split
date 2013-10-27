0b3be2a12e1624d06822192ddce86a3318f706a4
Cosmin Gheorghe <gcosmin@mit.edu>
2013-10-27 11:53:56 -0400

    Added validators to new purchases and payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,4 +3,21 @@ class Payment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :event
 
+	validates :description, :presence => true
+	validates :amount, :presence => true
+
+	validate :validate_event_selection
+	validate :validate_amount
+
+	def validate_event_selection
+		unless self.event
+			self.errors.add(:event, 'must be chosen.')
+		end
+	end
+
+	def validate_amount
+		unless self.amount and self.amount != 0
+			self.errors.add(:amount, 'must not be 0.')
+		end
+	end
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,22 @@
 class Purchase < ActiveRecord::Base
-
 	belongs_to :user
 	belongs_to :event
 
+	validates :description, :presence => true
+	validates :amount, :presence => true
+
+	validate :validate_event_selection
+	validate :validate_amount
+
+	def validate_event_selection
+		unless self.event
+			self.errors.add(:event, 'must be chosen.')
+		end
+	end
+
+	def validate_amount
+		if self.amount == 0
+			self.errors.add(:amount, 'must not be 0.')
+		end
+	end
 end
